2c50ceb2682488f12853a849246aa9b5a32bec2c
mmustafa56 <muhammadmustafapak71@gmail.com>
2024-11-24 15:53:08 +0400

    create home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='bg-slate-500 min-h-10 min-w-10'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet adipisci totam voluptatem ipsum, aliquam vitae rerum dolorem eius qui ex harum ab molestiae itaque, asperiores aperiam numquam delectus voluptates. Maxime.
-    
+    <div className=''>
+
     </div>
   )
 }
