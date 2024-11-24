c3de485e704318fad0bfa2fb2d7030878c0c3eea
mmustafa56 <muhammadmustafapak71@gmail.com>
2024-11-24 15:07:45 +0400

    create favorite and home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='bg-slate-500 min-h-10 min-w-10'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet adipisci totam voluptatem ipsum, aliquam vitae rerum dolorem eius qui ex harum ab molestiae itaque, asperiores aperiam numquam delectus voluptates. Maxime.
+    
+    </div>
+  )
+}
+
+export default Home
